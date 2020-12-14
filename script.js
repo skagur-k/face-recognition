09ce92283068bd96cd12122250e4f029a279b990
Nam Hyuck James, Kim <skagur0329@gmail.com>
2020-12-14 20:31:06 +0900

    Add model and edit script

---
+++ script.js
@@ -0,0 +1,24 @@
+const video = document.getElementById('video')
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+]).then(startVideo)
+
+function startVideo() {
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            video.srcObject = stream;
+        })
+        .catch(function (err0r) {
+            console.log("Something went wrong!");
+        });
+    }
+}
+
+video.addEventListener('play', () => {
+    console.log("asasd")
+})
